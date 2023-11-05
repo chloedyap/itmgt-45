4c842aabbfe8bf02ffb7a794d33f927943d53518
chloedyap <142221021+chloedyap@users.noreply.github.com>
2023-11-05 22:38:54 +0800

    Python Intermediate Assign commit

---
+++ mod-4-intermediate.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Intermediate Individual Assignment
+
+# In[22]:
+
+
+def shift_letter(letter, shift):
+    '''Shift Letter. 
+    4 points.
+    
+    Shift a letter right by the given number.
+    Wrap the letter around if it reaches the end of the alphabet.
+
+    Examples:
+    shift_letter("A", 0) -> "A"
+    shift_letter("A", 2) -> "C"
+    shift_letter("Z", 1) -> "A"
+    shift_letter("X", 5) -> "C"
+    shift_letter(" ", _) -> " "
+
+    *Note: the single underscore `_` is used to acknowledge the presence
+        of a value without caring about its contents.
+
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    shift: int
+        the number by which to shift the letter. 
+
+    Returns
+    -------
+    str
+        the letter, shifted appropriately, if a letter.
+        a single space if the original letter was a space.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
+    if letter == " ":
+        return " "
+    else:
+        shifted_ascii = ord(letter) + shift
+        if shifted_ascii > ord("Z"):
+            shifted_ascii -= 26
+        return chr(shifted_ascii)
+    
+print(shift_letter("A", 0))
+print(shift_letter("A", 2)) 
+print(shift_letter("Z", 1)) 
+print(shift_letter("X", 5)) 
+print(shift_letter(" ", 10)) 
+
+
+# In[21]:
+
+
+def caesar_cipher(message, shift):
+    '''Caesar Cipher. 
+    6 points.
+    
+    Apply a shift number to a string of uppercase English letters and spaces.
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    shift: int
+        the number by which to shift the letters. 
+
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
+    cipher_message = ""
+    
+    for letter in message:
+        if letter.isupper():
+            shifted_ascii = ord(letter) + shift
+            if shifted_ascii > ord("Z"):
+                shifted_ascii -= 26
+            cipher_message += chr(shifted_ascii)
+        elif letter == " ":
+            cipher_message += " "
+    return cipher_message
+
+caesar_cipher("HELLO WORLD", 1)
+
+
+# In[41]:
+
+
+def shift_by_letter(letter, letter_shift):
+    '''Shift By Letter. 
+    4 points.
+    
+    Shift a letter to the right using the number equivalent of another letter.
+    The shift letter is any letter from A to Z, where A represents 0, B represents 1, 
+        ..., Z represents 25.
+
+    Examples:
+    shift_by_letter("A", "A") -> "A"
+    shift_by_letter("A", "C") -> "C"
+    shift_by_letter("B", "K") -> "L"
+    shift_by_letter(" ", _) -> " "
+
+    Parameters
+    ----------
+    letter: str
+        a single uppercase English letter, or a space.
+    letter_shift: str
+        a single uppercase English letter.
+
+    Returns
+    -------
+    str
+        the letter, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
+    alphabet = [chr(i) for i in range(65, 91)]
+    
+    if letter == " ":
+        return " "
+    else:
+        final_letter = (alphabet.index(letter) + alphabet.index(letter_shift)) % 26
+        return(alphabet[final_letter])
+    
+print(shift_by_letter("A", "W"))
+print(shift_by_letter("B", "X")) 
+print(shift_by_letter("C", "Y")) 
+print(shift_by_letter(" ", _)) 
+print(shift_by_letter("L", "Y")) 
+
+
+# In[1]:
+
+
+def vigenere_cipher(message, key):
+    '''Vigenere Cipher. 
+    6 points.
+    
+    Encrypts a message using a keyphrase instead of a static number.
+    Every letter in the message is shifted by the number represented by the 
+        respective letter in the key.
+    Spaces should be ignored.
+
+    Example:
+    vigenere_cipher("A C", "KEY") -> "K A"
+
+    If needed, the keyphrase is extended to match the length of the key.
+        If the key is "KEY" and the message is "LONGTEXT",
+        the key will be extended to be "KEYKEYKE".
+
+    Parameters
+    ----------
+    message: str
+        a string of uppercase English letters and spaces.
+    key: str
+        a string of uppercase English letters. Will never be longer than the message.
+        Will never contain spaces.
+
+    Returns
+    -------
+    str
+        the message, shifted appropriately.
+    '''
+    # Replace `pass` with your code. 
+    # Stay within the function. Only use the parameters as input. The function should return your answer.
+    
+    alphabet = [chr(i) for i in range(65, 91)]
+    
+    key = key * (len(message) // len(key)) + key[:len(message) % len(key)]
+    result = ""
+
+    for i in range(len(message)):
+        if message[i] == ' ':
+            result += " "
+        else:
+            message_index = alphabet.index(message[i])
+            key_index = alphabet.index(key[i])
+            
+            encrypted_letter_index = (message_index + key_index) % 26
+            result += alphabet[encrypted_letter_index]
+
+    return result
+
+print(vigenere_cipher("A C", "KEY"))
+print(vigenere_cipher("ABCD", "WXYZ"))
+print(vigenere_cipher("HELLO WORLD", "KEY"))
+
